e2f8694eb91c1aa1ed4f0b1ef92415af9cbd5cbf
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-10-23 01:47:16 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,27 @@
 import React from "react";
 import "../styles/App.css";
-//import Paragraph from "./Paragraph";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      paragraph: ""
+      clicked: false
     };
   }
   setParagraph = () => {
-    this.setState({
-      paragraph:
-        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-    });
-    let container = document.getElementById("container");
-    if (container.childElementCount === 2) return;
-    let p = document.createElement("p");
-    p.setAttribute("id", "para");
-    p.innerHTML = this.state.paragraph;
-    container.appendChild(p);
+    this.setState({ clicked: true });
   };
   render() {
     return (
       <div id="main">
-        <div id="container">
-          <button id="click" onClick={this.setParagraph}>
-            Click
-          </button>
-        </div>
+        <button id="click" type="button" onClick={this.setParagraph}>
+          Click
+        </button>
+        {this.state.clicked && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
       </div>
     );
   }
